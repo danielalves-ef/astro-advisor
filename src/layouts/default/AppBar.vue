bc9ed9318e2b0961aad43540d5fb878113e7af55
Daniel Alves <danielalves.ef@gmail.com>
2024-03-15 19:07:33 +0100

    app start

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar flat>
+        <v-app-bar-title>
+            <v-icon icon="mdi-rocket" />
+
+            Astro Advisor
+        </v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
